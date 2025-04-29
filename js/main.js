5a637ffb4ec4e66737635b7730c80afe88b0484b
Nasir Uddin <74647944+nasiruddin-an4@users.noreply.github.com>
2025-04-29 11:32:23 +0600

    Add "Back to Top" button with functionality and responsive styles

--- js/main.js
+++ js/main.js
@@ -263,3 +263,22 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+
+
+const backToTopBtn = document.getElementById("backToTopBtn");
+
+  window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      backToTopBtn.style.display = "block";
+    } else {
+      backToTopBtn.style.display = "none";
+    }
+  };
+
+  backToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
